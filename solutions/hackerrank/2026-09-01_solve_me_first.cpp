bc0a293f2f54ed73e1349c759e9e0415a6b26abd
Varshitha61 <varshithasomashekar22@gmail.com>
2026-09-03 10:40:58 +0530

    Removed unused solution folders

--- solutions/hackerrank/2026-09-01_solve_me_first.cpp
+++
@@ -1,9 +0,0 @@
-#include <bits/stdc++.h>
-using namespace std;
-int main() {
-    long long a, b;
-    if (!(cin >> a)) return 0;
-    if (!(cin >> b)) return 0;
-    cout << a + b;
-    return 0;
-}
--- solutions/leetcode_2/2026-09-02_construct_uniform_parity_array_i.cpp
+++
@@ -1,9 +0,0 @@
-#include <bits/stdc++.h>
-using namespace std;
-
-class Solution {
-public:
-    bool constructArray(vector<int>& nums1) {
-        return true;
-    }
-};
