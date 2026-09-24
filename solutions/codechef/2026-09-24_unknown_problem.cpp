cd00f34da2673318cccebbe825a8072f4b4630ae
shanks5017 <kit27.cse53@gmail.com>
2026-09-24 13:40:29 +0000

    chore: automate daily coding bot run [skip ci]

---
+++ solutions/codechef/2026-09-24_unknown_problem.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int T;
+    if(!(cin >> T)) return 0;
+    while (T--) {
+        long long X, Y;
+        cin >> X >> Y;
+        long long diff = Y - X;
+        long long ans = 0;
+        if (diff > 0) ans = (diff + 7) / 8;
+        cout << ans;
+        if (T) cout << '\n';
+    }
+    return 0;
+}
